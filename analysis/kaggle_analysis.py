219c90d4313685bf72ced3352b7e4df0eb5496ff
UncleH25 <oneilljarryd@gmail.com>
2025-05-15 18:44:43 +0200

    Added a function that will load the kaggle dataset, with a try except block that will try to read and load the dataset, otherwise it will throw an error and display the error.

--- analysis/kaggle_analysis.py
+++ analysis/kaggle_analysis.py
@@ -1,3 +1,17 @@
 #imports
 import pandas as pd
 import os
+
+#Dataset path
+kaggle_dataset_path = os.path.join(os.path.dirname(__file__), 'data', 'data.csv')
+
+#Function to load the dataset
+def load_kaggle_data(kaggle_dataset_path):
+    #Try to load the dataset
+    try:
+        df = pd.read_csv(kaggle_dataset_path, encoding='ISO-8859-1')
+        return df
+    #If there is an error, print the error message
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        return None
