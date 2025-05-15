bda6e0c86af3afb15761066fd8844934fcb821a6
UncleH25 <oneilljarryd@gmail.com>
2025-05-15 19:04:21 +0200

    Add if-elif-else statements to the main function, which will choose the output once the user inputs, with basic error handling. Also added an if statement to run the main function only if this script is executed directly (not imported as a module).

--- main.py
+++ main.py
@@ -10,3 +10,21 @@ def main():
     print("1. Analyse Kaggle dataset")
     print("2. Analyse FNB dataset")
     choice = input("Choose dataset to analyze (1 or 2): ")
+
+    #If user chooses Kaggle dataset
+    if choice == "1":
+        kaggle_path = os.path.join("data", "ecommerce_data.csv")
+        df = load_kaggle_data(kaggle_path)
+        analyse_kaggle_data(df)
+    #If user chooses FNB dataset
+    elif choice == "2":
+        fnb_path = os.path.join("data", "dq_ps_challenge_v2.csv")
+        df = load_fnb_data(fnb_path)
+        analyse_fnb_data(df)
+    #If user chooses invalid option
+    else:
+        print("Invalid option.")
+
+#Run the main function only if this script is executed directly (not imported as a module)
+if __name__ == "__main__":
+    main()
