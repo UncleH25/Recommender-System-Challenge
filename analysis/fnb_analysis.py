599320bb79a006c70b8b55382a1f1ea0a06b9667
UncleH25 <oneilljarryd@gmail.com>
2025-05-15 18:53:02 +0200

    Added the fnb dataset file path, and as well as a function to load the dataset with a try try except block, whereby it will try to read the csv file, otherwise it will throw an error and display the error.

--- analysis/fnb_analysis.py
+++ analysis/fnb_analysis.py
@@ -1,3 +1,17 @@
 #imports
 import pandas as pd
 import os
+
+#Dataset path
+fnb_dataset_path = os.path.join(os.path.dirname(__file__), 'data', 'dq_ps_challenge_v2 1.csv')
+
+#Function to load the dataset
+def load_fnb_data(fnb_dataset_path):
+    # Try to load the dataset
+    try:
+        df = pd.read_csv(fnb_dataset_path)
+        return df
+    # If there is an error, print the error message
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        return None
