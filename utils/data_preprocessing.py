cb00c178340deb44b8c6fce1a5a4a9b1db7bdc82
UncleH25 <oneilljarryd@gmail.com>
2025-05-17 09:51:30 +0200

    Added a function that will preprocess the kaggle data, by firstly making a copy, then creating new columns, then encoding the CustomerID and StockCode as integers and storing them in their respective new colums. Also added a Basic Interaction Score Map, with DISPLAY, CLICK, and CHECKOUT being the basis of the interaction map.

--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -1,3 +1,28 @@
 #imports
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+
+#Function to preprocess the kaggle data
+def preprocess_kaggle_data(df):
+    """
+    Basic preprocessing for Kaggle dataset:
+    - Encode CustomerID and StockCode
+    - Return modified DataFrame and encoders
+    """
+
+    #Make a copy of the kaggle data
+    df = df.copy()
+
+    #Encode CustomerID and StockCode
+    user_coder = LabelEncoder()
+    item_coder = LabelEncoder()
+
+    #Encode the CustomerID and StockCode as integers and store them in their new columns
+    df['user_id'] = user_coder.fit_transform(df['CustomerID'])
+    df['item_id'] = item_coder.fit_transform(df['StockCode'])
+
+    #Basic interaction score mapping
+    interaction_map = {'DISPLAY': 1, 'CLICK': 2, 'CHECKOUT': 3}
+    df['interaction_score'] = df['Interaction'].map(interaction_map)
+
+    return df, user_coder, item_coder
