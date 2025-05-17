03bcf0d34eb37396e2e15fb9b9fedee237fc45a2
UncleH25 <oneilljarryd@gmail.com>
2025-05-17 16:51:37 +0200

    Spelling off.

--- recommenders/popularity_based.py
+++ recommenders/popularity_based.py
@@ -9,12 +9,12 @@ def get_top_kaggle_items(df, top_n=10):
     """
 
     #Group item popularity by product code and sum the quantity sold and sort the values in descending order
-    item_popularity = df.groupby('StockCode')['Quantity'].sum().sort_values(ascending=False)
+    kaggle_item_popularity = df.groupby('StockCode')['Quantity'].sum().sort_values(ascending=False)
     #Get the top N items
-    return item_popularity.head(top_n)
+    return kaggle_item_popularity.head(top_n)
 
 #Function to get the top N items for fnb dataset
-def get_top_fnb_tems(df, top_n=10):
+def get_top_fnb_items(df, top_n=10):
     """
     Recommends top-N most frequently CHECKOUTed items from FNB dataset.
     """
@@ -22,6 +22,6 @@ def get_top_fnb_tems(df, top_n=10):
     #Get the top N items based on the CHECKOUT interaction
     df_checkout = df[df['interaction'] == 'CHECKOUT']
     #Count the number of CHECKOUT interactions for each item
-    item_popularity = df_checkout['item'].value_counts().head(top_n)
+    fnb_item_popularity = df_checkout['item'].value_counts().head(top_n)
     #Return the top N items
-    return item_popularity
+    return fnb_item_popularity
