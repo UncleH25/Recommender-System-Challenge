9efba236d950509e192957263ea3339941739cef
UncleH25 <oneilljarryd@gmail.com>
2025-05-18 11:55:35 +0200

    Made a couple changes. Moved the os part mentioned earlier to the very beginning to avoid performance issues, and made the matrix csc instead.

--- recommenders/collaborative_filtering_fnb.py
+++ recommenders/collaborative_filtering_fnb.py
@@ -1,9 +1,9 @@
 #imports
+import os
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
 import pandas as pd
 import scipy.sparse as sparse
 from implicit.als import AlternatingLeastSquares
-import os
-os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
 #Function to build Interaction Matrix
 def build_interaction_matrix(df, user_col='user_id', item_col='item_id', rating_col='interaction_score'):
@@ -11,7 +11,7 @@ def build_interaction_matrix(df, user_col='user_id', item_col='item_id', rating_
     Create a sparse user-item matrix for training
     """
     #Create a sparse matrix from the DataFrame
-    return sparse.csr_matrix(
+    return sparse.csc_matrix(
         (df[rating_col], (df[user_col], df[item_col]))
     )
 
