2c50988b8a36552e88137d7e734e055b39c7cb60
UncleH25 <oneilljarryd@gmail.com>
2025-05-18 11:37:03 +0200

    Added a function that will build an Interaction Matrix, whereby it creates a sparse matrix from the dataframe.

--- recommenders/collaborative_filtering.py
+++ recommenders/collaborative_filtering.py
@@ -2,3 +2,13 @@
 import pandas as pd
 import scipy.sparse as sparse
 from implicit.als import AlternatingLeastSquares
+
+#Function to build Interaction Matrix
+def build_interaction_matrix(df, user_col='user_id', item_col='item_id', rating_col='interaction_score'):
+    """
+    Create a sparse user-item matrix for training
+    """
+    #Create a sparse matrix from the DataFrame
+    return sparse.csr_matrix(
+        (df[rating_col], (df[user_col], df[item_col]))
+    )
